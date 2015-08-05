f390ecf7b74cf13074042d3993998a987bb50154
Marcus Rowe <undisbeliever@gmail.com>
2015-08-05 13:15:57 +1000

    feat: create diff backup.

--- unbackup.py
+++ unbackup.py
@@ -26,7 +26,7 @@ CONFIG_VARIABLES = (
 )
 
 CONFIG_LIST_VARIABLES = (
-    'include',
+    'backup',
     'exclude',
 )
 
@@ -75,10 +75,10 @@ def verify_config(config):
     if 'backupdir' not in config:
         raise KeyError("Missing backupdir")
 
-    if 'include' not in config:
-        raise KeyError("Missing include")
+    if 'backup' not in config:
+        raise KeyError("Missing backup section")
 
-    for i in config['include']:
+    for i in config['backup']:
         if i[0] == '!' and not os.path.exists(i):
             raise IOError("File or directory does not exist", i)
 
@@ -87,6 +87,12 @@ def verify_config(config):
         raise ValueError("mx config must be between 0 and 9")
 
     return config
+
+
+
+def timestamp():
+    return datetime.datetime.now().strftime("%Y%m%d%H%M")
+
 
 
 def get_backup_directory(config):
@@ -99,26 +105,43 @@ def get_backup_directory(config):
     raise IOError("Cannot find backup directory")
 
 
-def full_backup(config):
-    dt = datetime.datetime.now().strftime("%y%m%d%H%M")
 
+def most_recent_full_backup(config):
+    bd = get_backup_directory(config)
+
+    regex = re.compile(r'^' + re.escape(config['name'] + '-') + r'\d{12}\.7z$')
+
+    filename = None
+
+    fnlist = os.listdir(bd)
+    fnlist.sort()
+
+    for fn in fnlist:
+        if regex.match(fn):
+            filename = os.path.join(bd, fn)
+
+    if not filename:
+        raise IOError("Cannot find a full backup", bd)
+
+    return filename
+
+
+
+def full_backup(config):
     backup_dir = get_backup_directory(config)
-    backup_file = os.path.join(backup_dir, config['name'] + '-full-' + dt + '.7z')
+    backup_file = os.path.join(backup_dir, config['name'] + '-' + timestamp() + '.7z')
 
     pargs = [
         config['7zip'],
-        'a',
+        'a', backup_file,
         '-t7z', '-mx' + str(config['mx']),
     ]
 
     for i in config['exclude']:
         pargs.append('-xr!' + i)
 
-    for i in config['include']:
-        pargs.append('-ir!' + i)
-        pass
-
-    pargs.append(backup_file)
+    for i in config['backup']:
+        pargs.append(i)
 
     r = subprocess.call(pargs, shell=False)
 
@@ -128,14 +151,52 @@ def full_backup(config):
     return backup_file
 
 
+
+def diff_backup(config):
+    full_bfn = most_recent_full_backup(config)
+
+    backup_dir = get_backup_directory(config)
+    backup_file = full_bfn[0:-3] + '-diff-' + timestamp() + '.7z'
+
+    pargs = [
+        config['7zip'],
+        'u', full_bfn,
+        '-t7z', '-mx' + str(config['mx']),
+    ]
+
+    for i in config['exclude']:
+        pargs.append('-xr!' + i)
+
+    for i in config['backup']:
+        pargs.append(i)
+
+    # ::KUDOS Javier - https://superuser.com/a/948380::
+    pargs.append('-u-')                             # Don't update the full backup
+    pargs.append('-up0q3r2x2y2z0w2!' + backup_file) # only write files that changed
+
+    r = subprocess.call(pargs, shell=False)
+
+    if r > 1:
+        raise Exception("Error with 7zip")
+
+    return backup_file
+
+
+
 def main(argv):
     config = read_config(argv[1])
-    full_backup(config)
+
+    if argv[2] == 'full':
+        full_backup(config)
+    elif argv[2] == 'diff':
+        diff_backup(config)
+    else:
+        raise ValueError("Excepected full or diff")
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        sys.stderr.write("Usage: {} <config file>\n".format(sys.argv[0]))
+    if len(sys.argv) != 3:
+        sys.stderr.write("Usage: {} <config file> <full|diff>\n".format(sys.argv[0]))
     else:
         main(sys.argv)
 
